d85e8b64c54a1d3b0792e3d39e50a87b5933be17
fghjdeg <168495703+fghjdeg@users.noreply.github.com>
2024-07-09 10:00:30 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -10,3 +10,7 @@ let sqrt2 = Math.sqrt(2);
 let sqrt3 = Math.sqrt(3);
 let sumSqrt = sqrt2 + sqrt3;
 console.log("2の平方根と3の平方根の和: " + sumSqrt);
+let divideOneByZero = 1 / 0;
+console.log("1を0で割ると: " + divideOneByZero);
+let divideZeroByZero = 0 / 0;
+console.log("0を0で割ると: " + divideZeroByZero);
