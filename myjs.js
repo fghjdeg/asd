ffe2e3cdce54da928d9b3e55e1b62eed23d491fb
fghjdeg <168495703+fghjdeg@users.noreply.github.com>
2024-07-09 09:59:14 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -6,6 +6,7 @@ alert(Math.sqrt(1+1));
 Number('2')+Number('3')
 '2'+3
 Number('2')+3
-let sqrt2=Math.sqrt(2);
-let sqrt3=Math.sqrt(3);
-let sumSqrt=sqrt2+sqrt3
+let sqrt2 = Math.sqrt(2);
+let sqrt3 = Math.sqrt(3);
+let sumSqrt = sqrt2 + sqrt3;
+console.log("2の平方根と3の平方根の和: " + sumSqrt);
